1afc251ed8741d87c7033a1d56f78e4a738fa6cd
Andrew Clissold <andrewclissold@gmail.com>
2015-08-02 12:34:43 -0400

    Use After module

--- main.swift
+++ main.swift
@@ -1,14 +1,7 @@
 import Foundation
 
-// import After // aclissold/After
+import After // aclissold/After:v1.0.0
 import Blocker // aclissold/Blocker:v1.0.2
-
-func after(seconds: Double, closure: () -> ()) {
-    let delay = dispatch_time(DISPATCH_TIME_NOW,
-                              Int64(seconds * Double(NSEC_PER_SEC)))
-    let mainQueue = dispatch_get_main_queue()
-    dispatch_after(delay, mainQueue, closure)
-}
 
 func main() {
     defer {
